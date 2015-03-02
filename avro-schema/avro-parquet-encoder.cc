7f05068c2f9a0e092c78db07aabee86cc8e1e1ea
Neal Sidhwaney <nealsid@gmail.com>
2015-03-02 16:53:09 -0500

    More boilerplate for avro->parquet encoder
    Not quite wired up fully, but close.

--- avro-schema/avro-parquet-encoder.cc
+++ avro-schema/avro-parquet-encoder.cc
@@ -1,1 +1,140 @@
 #include "avro-parquet-encoder.h"
+#include "avro-schema/avro-schema-walker.h"
+
+using parquet_file::AvroSchemaWalker;
+using parquet_file::AvroParquetEncoder;
+
+namespace parquet_file {
+
+AvroParquetEncoder::AvroParquetEncoder(std::string json_schema_filename) {
+  avro_schema_walker_.reset(new AvroSchemaWalker(json_schema_filename));
+}
+
+AvroParquetEncoder::~AvroParquetEncoder() {
+}
+
+void AvroParquetEncoder::init(avro::OutputStream& os) {
+
+}
+
+/// Flushes any data in internal buffers.
+void AvroParquetEncoder::flush() {
+
+}
+
+/// Encodes a null to the current stream.
+void AvroParquetEncoder::encodeNull() {
+
+}
+
+/// Encodes a bool to the current stream
+void AvroParquetEncoder::encodeBool(bool b) {
+
+}
+
+/// Encodes a 32-bit int to the current stream.
+void AvroParquetEncoder::encodeInt(int32_t i) {
+
+}
+
+/// Encodes a 64-bit signed int to the current stream.
+void AvroParquetEncoder::encodeLong(int64_t l) {
+
+}
+
+/// Encodes a single-precision floating point number to the current stream.
+void AvroParquetEncoder::encodeFloat(float f) {
+
+}
+
+/// Encodes a double-precision floating point number to the current stream.
+void AvroParquetEncoder::encodeDouble(double d) {
+
+}
+
+/// Encodes a UTF-8 string to the current stream.
+void AvroParquetEncoder::encodeString(const std::string& s) {
+
+}
+
+/**
+ * Encodes aribtray binary data into tthe current stream as Avro "bytes"
+ * data type.
+ * \param bytes Where the data is
+ * \param len Number of bytes at \p bytes.
+ */
+void AvroParquetEncoder::encodeBytes(const uint8_t *bytes, size_t len) {
+
+}
+
+/**
+ * Encodes aribtray binary data into tthe current stream as Avro "bytes"
+ * data type.
+ * \param bytes The data.
+ */
+void AvroParquetEncoder::encodeBytes(const std::vector<uint8_t>& bytes) {
+  uint8_t b;
+  encodeBytes(bytes.empty() ? &b : &bytes[0], bytes.size());
+}
+
+// Encodes fixed length binary to the current stream.
+void AvroParquetEncoder::encodeFixed(const uint8_t *bytes, size_t len) {
+
+}
+
+void AvroParquetEncoder::encodeFixed(const std::vector<uint8_t>& bytes) {
+  encodeFixed(&bytes[0], bytes.size());
+}
+
+/// Encodes enum to the current stream.
+void AvroParquetEncoder::encodeEnum(size_t e) {
+
+}
+
+/// Indicates that an array of items is being encoded.
+void AvroParquetEncoder::arrayStart() {
+
+}
+
+/// Indicates that the current array of items have ended.
+void AvroParquetEncoder::arrayEnd() {
+
+}
+
+/// Indicates that a map of items is being encoded.
+void AvroParquetEncoder::mapStart() {
+
+}
+
+/// Indicates that the current map of items have ended.
+void AvroParquetEncoder::mapEnd() {
+
+}
+
+/// Indicates that count number of items are to follow in the current array
+/// or map.
+void AvroParquetEncoder::setItemCount(size_t count) {
+
+}
+
+/// Marks a beginning of an item in the current array or map.
+void AvroParquetEncoder::startItem() {
+
+}
+
+/// Encodes a branch of a union. The actual value is to follow.
+void AvroParquetEncoder::encodeUnionIndex(size_t e) {
+
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  if (argc < 2) {
+    LOG(FATAL) <<
+      "Specify JSON schema file on command line";
+    return 1;
+  }
+  AvroParquetEncoder encoder(argv[1]);
+}
--- avro-schema/avro-parquet-encoder.h
+++ avro-schema/avro-parquet-encoder.h
@@ -1,36 +1,48 @@
+#ifndef PARQUET_FILE_AVRO_PARQUET_ENCODER_H_
+#define PARQUET_FILE_AVRO_PARQUET_ENCODER_H_
+
 #include <avro/Encoder.hh>
+
+namespace parquet_file {
+
+class AvroSchemaWalker;
 
 class AvroParquetEncoder : public avro::Encoder {
  public:
-  virtual ~AvroParquetEncoder() { };
+  AvroParquetEncoder(std::string json_schema_filename);
+ private:
+  std::unique_ptr<AvroSchemaWalker> avro_schema_walker_;
+
+ public:
+  ~AvroParquetEncoder();
   /// All future encodings will go to os, which should be valid until
   /// it is reset with another call to init() or the encoder is
   /// destructed.
-  virtual void init(avro::OutputStream& os);
+  void init(avro::OutputStream& os);
 
   /// Flushes any data in internal buffers.
-  virtual void flush();
+  void flush();
 
   /// Encodes a null to the current stream.
-  virtual void encodeNull();
+  void encodeNull();
 
   /// Encodes a bool to the current stream
-  virtual void encodeBool(bool b);
+  void encodeBool(bool b);
 
   /// Encodes a 32-bit int to the current stream.
-  virtual void encodeInt(int32_t i);
+  void encodeInt(int32_t i);
 
   /// Encodes a 64-bit signed int to the current stream.
-  virtual void encodeLong(int64_t l);
+  void encodeLong(int64_t l);
 
   /// Encodes a single-precision floating point number to the current stream.
-  virtual void encodeFloat(float f);
+  void encodeFloat(float f);
 
   /// Encodes a double-precision floating point number to the current stream.
-  virtual void encodeDouble(double d);
+  void encodeDouble(double d);
 
   /// Encodes a UTF-8 string to the current stream.
-  virtual void encodeString(const std::string& s);
+  void encodeString(const std::string& s);
 
   /**
    * Encodes aribtray binary data into tthe current stream as Avro "bytes"
@@ -38,52 +50,51 @@ class AvroParquetEncoder : public avro::Encoder {
    * \param bytes Where the data is
    * \param len Number of bytes at \p bytes.
    */
-  virtual void encodeBytes(const uint8_t *bytes, size_t len);
+  void encodeBytes(const uint8_t *bytes, size_t len);
 
   /**
    * Encodes aribtray binary data into tthe current stream as Avro "bytes"
    * data type.
    * \param bytes The data.
    */
-  void encodeBytes(const std::vector<uint8_t>& bytes) {
-    uint8_t b;
-    encodeBytes(bytes.empty() ? &b : &bytes[0], bytes.size());
-  }
+  void encodeBytes(const std::vector<uint8_t>& bytes);
 
   /// Encodes fixed length binary to the current stream.
-  virtual void encodeFixed(const uint8_t *bytes, size_t len);
+  void encodeFixed(const uint8_t *bytes, size_t len);
 
   /**
    * Encodes an Avro data type Fixed.
    * \param bytes The fixed, the length of which is taken as the size
    * of fixed.
    */
-  void encodeFixed(const std::vector<uint8_t>& bytes) {
-    encodeFixed(&bytes[0], bytes.size());
-  }
+  void encodeFixed(const std::vector<uint8_t>& bytes);
 
   /// Encodes enum to the current stream.
-  virtual void encodeEnum(size_t e);
+  void encodeEnum(size_t e);
 
   /// Indicates that an array of items is being encoded.
-  virtual void arrayStart();
+  void arrayStart();
 
   /// Indicates that the current array of items have ended.
-  virtual void arrayEnd();
+  void arrayEnd();
 
   /// Indicates that a map of items is being encoded.
-  virtual void mapStart();
+  void mapStart();
 
   /// Indicates that the current map of items have ended.
-  virtual void mapEnd();
+  void mapEnd();
 
   /// Indicates that count number of items are to follow in the current array
   /// or map.
-  virtual void setItemCount(size_t count);
+  void setItemCount(size_t count);
 
   /// Marks a beginning of an item in the current array or map.
-  virtual void startItem();
+  void startItem();
 
   /// Encodes a branch of a union. The actual value is to follow.
-  virtual void encodeUnionIndex(size_t e);
+  void encodeUnionIndex(size_t e);
 };
+
+}  // namespace
+
+#endif  // #ifdef PARQUET_FILE_AVRO_PARQUET_ENCODER_H_
